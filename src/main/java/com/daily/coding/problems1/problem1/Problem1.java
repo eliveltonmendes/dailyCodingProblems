1ccdb61e9b6113671da1a88354dde6a4d98d91a8
Elivelton Mendes <eliveltonmendes14@gmail.com>
2021-04-19 16:43:00 -0300

    Testes automatizados

---
+++ src/main/java/com/daily/coding/problems1/problem1/Problem1.java
@@ -0,0 +1,15 @@
+package com.daily.coding.problems1.problem1;
+
+import java.util.List;
+
+public class Problem1 {
+    /**
+     * Problem
+     * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+     * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+     */
+    
+    public boolean calculateValue(List<Integer> numberList, Integer numberValue) {
+        return false;
+    }
+}
